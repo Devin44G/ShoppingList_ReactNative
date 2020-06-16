e7402cb8521629d1318dfd2fe1d460316e39674f
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-06-16 18:17:52 -0500

    Resolved uuid issues with React Native - Added addItems functionality to shopping list

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
+import uuid from 'uuid-random';
 
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
   const [items, setItems] = useState([
-    {id: 1, text: 'Milk'},
-    {id: 2, text: 'Tea'},
-    {id: 3, text: 'Sugar'},
-    {id: 4, text: 'Bread'},
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Tea'},
+    {id: uuid(), text: 'Sugar'},
+    {id: uuid(), text: 'Bread'},
   ]);
+
+  console.log(items);
 
   const deleteItem = (id) => {
     setItems(prevItems => {
       return prevItems.filter(item => item.id != id);
     });
+  };
+
+  const addItem = (item) => {
+    if(!item) {
+      Alert.alert('Error', 'Please enter an item', [{text: 'Ok'}]);
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text: item}, ...prevItems];
+      });
+    }
   }
 
   return(
     <View style={styles.container}>
       <Header />
+      <AddItem addItem={addItem}/>
       <FlatList
         data={items}
-        renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem} />}
+        renderItem={({ item }) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
